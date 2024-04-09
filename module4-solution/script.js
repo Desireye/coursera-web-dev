b8d34f40fb3b19153353521e94df623a0fca2d79
Modesire Soneye <67333381+Desireye@users.noreply.github.com>
2024-04-09 16:47:29 +0100

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,10 +1,11 @@
 function greeting() {
-    var names = ['Desire', 'Quinn', 'Jake', 'Nicole', 'Joan of Arc', 'Sunny', 'Yvonne', 'jin', 'jane'];
+    var names = ['Yaakov', 'John', 'Jen', 'Jason', 'Paul', 'Frank', 'Larry', 'Paula', ' Laura', 'Jim',
+                 'Desire', 'Quinn', 'Jake', 'Nicole', 'Joan of Arc', 'Sunny', 'Yvonne', 'jin', 'jane'];
 
     for (var i = 0; i < names.length; i++) {
-        const good = "J"
-        const bye = "j"
-        if(names[i].includes(good) || names[i].includes(bye)) {
+        const firstLetter = "J"
+        const firstLetter2 = "j"
+        if(names[i].includes(firstLetter) || names[i].includes(firstLetter2)) {
             console.log("Goodbye " + names[i]);
         } else {
             console.log("Hello " + names[i]);
